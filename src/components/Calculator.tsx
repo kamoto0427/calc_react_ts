132d4358f55979cb939fd527520d797839a0715f
kamoto <takahiro.08039@gmail.com>
2022-02-26 23:09:43 +0900

    電卓ボタンの作成

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,11 @@
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+
+export default function Calculator() {
+    return (
+        <div>
+            <Display />
+            <ButtonPanel />
+        </div>
+    );
+};
